ce981f48824c56a15c13e9fd0ed19fc0bd21059c
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-31 20:53:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,15 +5,14 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = open(index_url)
-    data = Nokogiri::HTML(html)
-    student = doc.css(".student-card a")
-    array = student.collect do |i|
-      {:name => element.css(".student-name").text , 
-      :location => element.css(".student-location").text, 
-      :profile_url => element.attr('href')
+    doc = Nokogiri::HTML(html)
+    student_cards = doc.css(".student-card a")
+    student_cards.collect do |element|
+      {:name => element.css(".student-name").text ,
+        :location => element.css(".student-location").text,
+        :profile_url => element.attr('href')
       }
     end
-    array
   end
 
   def self.scrape_profile_page(profile_url)
